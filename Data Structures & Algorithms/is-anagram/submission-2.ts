09bb987fb7c16acd277b6a9ab2cf00a06d50d745
Madiyar Kenzhebayev <110607110+Madikensky@users.noreply.github.com>
2026-05-14 15:04:30 +0500

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.ts
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+        if (s.length !== t.length) return false;
+        const obj = {};
+
+        for (let j = 0; j < s.length; j++) {
+            if (!obj[s[j]]) {
+                obj[s[j]] = 1;
+            } else {
+                obj[s[j]]++;
+            }
+        }
+
+        // r: 2, c: 2 a: 2, e: 1
+
+        for (let k = 0; k < t.length; k++) {
+            if (obj[t[k]] && obj[t[k]] > 0) {
+                console.log(t[k])
+                obj[t[k]]--;
+            } else {
+                return false;
+            }
+        }
+        return true
+    }
+}
