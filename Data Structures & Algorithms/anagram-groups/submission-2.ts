2d1d051954293e82d4edf509b42356b5cc2da288
Madiyar Kenzhebayev <110607110+Madikensky@users.noreply.github.com>
2026-05-26 15:13:30 +0500

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.ts
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs: string[]): string[][] {
+        const hmap = {};
+        for (let word = 0; word < strs.length; word++) {
+            const obj = {};
+            for (let char = 0; char < strs[word].length; char++) {
+                if (obj[strs[word][char]]) {
+                    obj[strs[word][char]]++;
+                } else {
+                    obj[strs[word][char]] = 1;
+                }
+            }
+            const formatted = Object.entries(obj).sort().toString()
+            console.log(formatted)
+            if (hmap[formatted]) {
+                hmap[formatted].push(strs[word]);
+            } else {
+                hmap[formatted] = [strs[word]]
+            }
+        }
+        return (Object.values(hmap))
+    }
+}
